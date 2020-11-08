ddc8bf9d9c8c02caa8999954ca23e055d08f9ee5
404NotFound <404NotFound.59@protonmail.com>
2020-11-08 14:22:29 -0500

    Added '/global' Command

---
+++ src/main/java/me/four04/perworldchat/commands/Global.java
@@ -0,0 +1,41 @@
+package me.four04.perworldchat.commands;
+
+import me.four04.perworldchat.PerWorldChat;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Global implements CommandExecutor {
+
+    private final String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + "Global" + ChatColor.DARK_GRAY + "] " + ChatColor.RESET;
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player && label.equalsIgnoreCase("global")) {
+
+            Player player = (Player) sender;
+
+            String permission = "pwc.global";
+            if (!sender.hasPermission(permission)) {
+                player.sendMessage(PerWorldChat.prefix + ChatColor.RED + "missing permission: '" + permission + "'");
+                return true;
+            }
+
+            if (args.length == 0) {
+                player.sendMessage(PerWorldChat.prefix + ChatColor.RED + "You must enter a message!");
+                return false;
+            }
+
+            String message = String.join("", args);
+            String broadcast = this.prefix + ChatColor.DARK_AQUA + player.getName() + ": " + ChatColor.RESET + message;
+
+            for (Player p : PerWorldChat.server.getOnlinePlayers()) {
+                p.sendMessage(broadcast);
+            }
+            return true;
+        }
+        return false;
+    }
+}
